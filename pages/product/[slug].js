5a571dad5024b80801e1e891604239f99a5c8627
emlegweak <emilythearmstrong@gmail.com>
2022-11-03 18:47:51 -0400

    updates to product slugs

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,5 +1,6 @@
 //file-based routing in next.js
 //square brackets around file names means that next.js will dynamically render the page based on the slug name
+//don't need to implement any kind of library like react-router, just create a new folder and write JSX
 import React from 'react'
 
 const ProductDetails = () => {
