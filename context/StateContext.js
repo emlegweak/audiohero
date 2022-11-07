186747c29df62cb15d46b2e781b2b7e9be5bb499
emlegweak <emilythearmstrong@gmail.com>
2022-11-07 18:03:39 -0500

    cart logic

--- context/StateContext.js
+++ context/StateContext.js
@@ -6,38 +6,57 @@ const Context = createContext()
 export const StateContext = ({children}) =>{
     const [showCart, setShowCart] = useState(false);
     const [cartItems, setCartItems] = useState([]);
-    const [totalPrice, setTotalPrice] = useState();
+    const [totalPrice, setTotalPrice] = useState(0);
     const [totalQuantities, setTotalQuantities] = useState(0);
     const [qty, setQty] = useState(1);
 
-       //cart functionality
-    const onAdd = (product, quantity) =>{
-        //is product already in cart?
-        const checkProductInCart = cartItems.find((item) => isValidElement._id === product._id)
+    let foundProduct 
+    let index
 
-        if(checkProductInCart){
-            setTotalPrice((prevTotalPrice) => prevTotalPrice + product.price * quantity)
-            setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + quantity)
+     //cart functionality - add to cart
+     const onAdd = (product, quantity) =>{
+        const checkProductInCart = cartItems.find((item) => item._id === product._id);
+    
+        setTotalPrice((prevTotalPrice) => prevTotalPrice + product.price * quantity);
+        setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + quantity);
+        
+        if(checkProductInCart) {
+          const updatedCartItems = cartItems.map((cartProduct) => {
+            if(cartProduct._id === product._id) return {
+              ...cartProduct,
+              quantity: cartProduct.quantity + quantity
+            }
+          })
 
-            const updatedCartItems = cartItems.map((cartProduct) =>{
-                if(cartProduct._id === product._id){
-                    return {
-                        ...cartProduct, 
-                        quantity: cartProduct.quantity + quantity
-                    }
-                }
-            })
-            setCartItems(updatedCartItems)
-           
-        }else{
-            product.quantity = quantity
-
-            setCartItems([...cartItems, {...product}])   
+          setCartItems(updatedCartItems);
+        } else {
+          product.quantity = quantity;
+        
+          setCartItems([...cartItems, { ...product }]);
         }
-         toast.success(`${qty} ${product.name} added to cart`)
+
+        toast.success(`${qty} ${product.name} added to the cart.`);
+  } 
+
+    //toggle cart item quantity
+    const toggleCartItemQuantity = (id, value) => {
+        foundProduct = cartItems.find((item) => item._id === product._id)
+        index = cartItems.findIndex((product) => product._id === id)
+        
+        if(val === 'inc'){
+            foundProduct.quantity += 1
+            setCartItems([...cartItems, {...foundProduct, quantity: foundProduct.quantity + 1}])
+            setTotalPrice((prevTotalPrice) => prevTotalPrice + foundProduct.price)
+            setTotalQuantities(prevTotalQuantities => prevTotalQuantities + 1)
+        }else if (val === 'dec'){
+            if (foundProduct.quantity > 1){
+            foundProduct.quantity += 1
+            setCartItems( [...cartItems, {...foundProduct, quantity: foundProduct.quantity - 1}])
+            setTotalPrice((prevTotalPrice) => prevTotalPrice - foundProduct.price)
+            setTotalQuantities(prevTotalQuantities => prevTotalQuantities - 1)
+            }
+        }
     }
-
-
 
     //dynamic quantity update functions
     //increase quantity
@@ -58,7 +77,7 @@ export const StateContext = ({children}) =>{
     return(
         <Context.Provider
             value={{
-                showCart, setShowCart, cartItems, totalPrice, totalQuantities, qty, incQty, decQty, onAdd
+                showCart, setShowCart, cartItems, totalPrice, totalQuantities, qty, incQty, decQty, onAdd, toggleCartItemQuantity
             }}
         >
             {children}
