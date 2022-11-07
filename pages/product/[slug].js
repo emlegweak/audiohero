01d693ddb8f69513577ae40447250f5f3b60dfc8
emlegweak <emilythearmstrong@gmail.com>
2022-11-07 12:22:11 -0500

    added business logic for updating product quantity, started logic for cart

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -5,12 +5,16 @@ import React, {useState} from 'react'
 import {client, urlFor} from '../../lib/client'
 import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-icons/ai'
 import { Product } from '../../components'
+import {useStateContext} from '../../context/StateContext'
 
 const ProductDetails = ({product, products}) => {
   const {image, name, details, price} = product
 
   //state mgmt for image-container
   const [index, setIndex] = useState(0)
+
+  //import state functions from StateContext.js
+  const {decQty, incQty, qty, onAdd} = useStateContext()
 
   return (
     <div>
@@ -47,22 +51,23 @@ const ProductDetails = ({product, products}) => {
               <h3>Quantity: </h3>
               <p className='quantity-desc'>
                 <span className='minus'
-                      onClick=''>
+                      onClick={decQty}>
                         <AiOutlineMinus />
                 </span>
                 <span className='num'
                       onClick=''>
-                        0
+                        {qty}
                 </span>
                 <span className='plus'
-                      onClick=''>
+                      onClick={incQty}>
                         <AiOutlinePlus />
                 </span>
               </p>
             </div>
             <div className='buttons'>
               <button type='button'
-                      className='add-to-cart'>
+                      className='add-to-cart'
+                      onClick={() => onAdd(product, qty)}>
                         Add to Cart
               </button>
                 <button type='button'
